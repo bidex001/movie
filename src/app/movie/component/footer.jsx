d70f7c5db94bd208592b0c53895dfad8c8f7af60
Adebayo Bidemi <adebayob173@gmail.com>
2026-04-01 17:10:11 +0100

    footer updated

--- src/app/movie/component/footer.jsx
+++ src/app/movie/component/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='flex w-full justify-center py-4 shadow-[-10px_0px_4px]'>
-        <h1 className='capitalize font-bold text-2xl font-inconsolata'>created by bidex </h1>
+        <h1 className='capitalize font-bold text-2xl font-inconsolata animate-shine'>created by bidex</h1>
     </div>
   )
 }
